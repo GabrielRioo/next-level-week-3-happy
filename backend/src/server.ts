c7a27d6103d595d379ec8ccbc72df7225a149750
GabrielRioo <Gabrielpaivario@gmail.com>
2020-11-01 00:10:31 -0300

    fix erros and upload images

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
+import './database/connection';
 
-import './database/connection' 
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/users', (req, res) => {
-    return res.json({message: "Hello World"});
-});
 
 app.listen(3333);
